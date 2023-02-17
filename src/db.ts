7dfaf29994379f0d6885687e55363d9b0b8575f6
Alina Rusu <alex.alina.rusu@gmail.com>
2023-02-17 11:59:51 -0500

    Run app in docker container with compose

--- src/db.ts
+++ src/db.ts
@@ -28,7 +28,7 @@ const parentDir = join(__dirname, '..');
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '0.0.0.0',
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
